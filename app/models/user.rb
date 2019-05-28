9649d54ed011d829e7339ef5f03d69b92eac151f
App Academy Student <g.thirteen333@gmail.com>
2019-05-28 17:45:00 -0400

    confirm user can save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, :password_digest, :session_token, presence: true
+    validates :username, :password_digest, :session_token, :email, presence: true
     validates :password, length:{minimum: 8}, allow_nil: true
     
     attr_reader :password
