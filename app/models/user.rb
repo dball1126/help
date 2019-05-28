7d7e4af5e03710e02aa480886ab964455fa7762f
App Academy Student <g.thirteen333@gmail.com>
2019-05-28 16:49:22 -0400

    null false added to session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     attr_reader :password
     after_initialize :ensure_session_token
 
-    def self.find_by_credentials(user, password)
+    def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
         if user && user.is_password?(password)
             return user
@@ -37,5 +37,4 @@ class User < ApplicationRecord
         self.save!
         self.session_token
     end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_181430) do
+ActiveRecord::Schema.define(version: 2019_05_28_200421) do
 
   create_table "users", force: :cascade do |t|
     t.string "password_digest", null: false
     t.string "username", null: false
     t.string "email", null: false
+    t.string "session_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
