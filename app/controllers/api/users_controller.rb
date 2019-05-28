78a5dbee2522e755e7f1f89aada0bdb283b53df7
App Academy Student <g.thirteen333@gmail.com>
2019-05-28 14:21:44 -0400

    created and migrated users

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,15 @@
+class Api::UsersController < ApplicationController
+    def new
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    private
+    def user_params
+        params.require(:
+    end
+end
