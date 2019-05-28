f6b2aa5e2a585df152d85bbc0ef0b2316a29c8d9
App Academy Student <g.thirteen333@gmail.com>
2019-05-28 17:29:13 -0400

    switch db from sqlite3 to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_28_200421) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "password_digest", null: false
     t.string "username", null: false
