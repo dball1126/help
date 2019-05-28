ebd81d782abdc1a4588a6a8f12572be43cb86318
App Academy Student <g.thirteen333@gmail.com>
2019-05-28 14:54:14 -0400

    added user model and users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,15 +1,25 @@
 class Api::UsersController < ApplicationController
+    
     def new
-    end
-
-    def show
+        @user = User.new
     end
 
     def create
+        @user = User.new(user_params)
+        if @user
+            login!(@user)
+        else
+            render json: @user.errors.full_messages
+        end
+    end
+
+
+    def show
+        @user
     end
 
     private
     def user_params
-        params.require(:
+        params.require(:user).permit(:username, :password);
     end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    helper_method: 
+
+    def login!(user)
+    end
+
+    def logout!
+    end
+
+    def ensure_logged_in?
+        redirect_to 
+    end
+
+    def current_user
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
