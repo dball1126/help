cfffb85261239c9fc17498e2bd1d08edebfc2d0b
daniel <g.thirteen333@gmail.com>
2019-06-04 11:09:59 -0400

    added some features to the business show page

--- frontend/components/business_show/business_show.jsx
+++ frontend/components/business_show/business_show.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Search from '../search/search';
 import RedBanner from '../greeting/top_banner_red';
+import {Link} from 'react-router-dom';
 class BusinessShow extends React.Component {
     constructor(props){
         super(props);
@@ -19,9 +20,55 @@ class BusinessShow extends React.Component {
         return (
             <div>
                 <RedBanner />
+                <div className="business-show-header">
+                    <div className="business-header-container">
+                        <div className="business-header-logo">
+                        </div>
+                        <div className="business-search-container">
+                            <form method="get" className="business-show-search">
+                                <div className="business-show-search-left">
+                                    <label htmlFor="" className="searchbar-left-label">
+                                        <div className="searchbar-left-inside">
+                                            <span className="find-searchbar-left">Find</span>
+                                            <span className="input-searchbar-left">
+                                                <input type="text"
+                                                    placeholder="ac repair, burgers, bars, spas..."
+                                                    className="business-search" />
+                                            </span>
+                                        </div>
+                                    </label>
+                                </div>
+                                <div className="business.show-search-right">
+                                    <label htmlFor="" className="searchbar-right-label">
+                                        <div className="searchbar-right-inside">
+                                            <span className="near-searchbar-right">Near</span>
+                                            <span className="input-searchbar-right">
+                                                <input type="text"
+                                                    placeholder="Manhattan, NY"
+                                                    className="location-search" />
+                                            </span>
+                                        </div>
+                                    </label>
+                                </div>
+                                <div className="business-show-search-button">
+                                    <button type="submit" className="search-button-business-show">
+                                        <div className="business-show-search-image"></div>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="business-show-login-signup">
+                            <div className="business-show-login-box">
+                            <Link to="/login" className="business-show-login">Log In</Link>
+                            </div>
+                            <div className="business-show-signup-box">
+                            <Link to="/signup" className="business-show-signup">Sign up</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 
-                
-                    <Search />
+                    
                     
                 
                 <ul className="ul-business-info">
