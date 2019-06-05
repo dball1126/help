e92e1236217806227bb0590400d4159ece58e527
daniel <g.thirteen333@gmail.com>
2019-06-05 16:24:43 -0400

    changed seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,10 @@ business3 = Business.create(name: "L & B Spumoni Gardens", state: "NY", city: "B
 # business3.images.attach(io: File.open('./app/assets/images/spumoni2.jpg'), filename: 'spumoni2.jpg')
 # business3.images.attach(io: File.open('./app/assets/images/spumoni3.jpg'), filename: 'spumoni3.jpg')
 file1 = open('https://yap-dev.s3.amazonaws.com/spumoni1.jpg')
-business3.images.attach(io: file, filename: 'spumoni3.jpg')
+business3.images.attach(io: file1, filename: 'spumoni3.jpg')
 file2 = open('https://yap-dev.s3.amazonaws.com/spumoni2.jpg')
-business3.images.attach(io: file, filename: 'spumoni3.jpg')
+business3.images.attach(io: file2, filename: 'spumoni3.jpg')
 file3 = open('https://yap-dev.s3.amazonaws.com/spumoni3.jpg')
-business3.images.attach(io: file, filename: 'spumoni3.jpg')
+business3.images.attach(io: file3, filename: 'spumoni3.jpg')
 
 
