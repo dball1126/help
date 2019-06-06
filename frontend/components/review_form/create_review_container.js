40bbcee54fb599d04df6543b13df384229dad519
daniel <g.thirteen333@gmail.com>
2019-06-06 00:49:49 -0400

    review containers complete and review form skeleton complete

--- frontend/components/review_form/create_review_container.js
+++ frontend/components/review_form/create_review_container.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import ReviewForm from './review_form';
+import {createReview} from '../../actions/review_actions';
+import {fetchBusiness} from '../../actions/business_actions';
 
 const mapStateToProps = (state) => {
     return {
-
+        review: {content: "", rating: 0},
+        formType: 'Create Review'
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-
+        fetchBusiness: (id) => dispatch(fetchBusiness(id)),
+        action: (review) => dispatch(createReview(review))
     }
 }
 
--- frontend/components/review_form/review_form.jsx
+++ frontend/components/review_form/review_form.jsx
@@ -4,14 +4,25 @@ import {Link} from 'react-router-dom';
 class ReviewForm extends React.Component{
     constructor(props){
         super(props);
-        this.state = {content: "", rating: null}
+        this.state = this.props.review;
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.action(this.state)
+    }
+
+    update(e){
+        return (e) => {
+            this.setState({[field]: e.target.value})
+        }
     }
 
     render(){
         return (
             <div>
-                
+                <p>{state.content}</p>
             </div>
         )
     }
