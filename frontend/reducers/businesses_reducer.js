de93b9dc24ad94c76d94eed5872951890b367ea8
daniel <g.thirteen333@gmail.com>
2019-06-07 12:02:24 -0400

    switched from merge to object.assign

--- frontend/reducers/businesses_reducer.js
+++ frontend/reducers/businesses_reducer.js
@@ -1,16 +1,16 @@
 import {RECEIVE_ALL_BUSINESSES, RECEIVE_BUSINESS} from '../actions/business_actions';
-import merge from 'lodash/merge';
+
 import {RECEIVE_REVIEW} from '../actions/review_actions';
 
 const businessesReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
-    let newState = merge({}, oldState);
+    let newState = Object.assign({}, oldState);
 
     switch (action.type) {
         case RECEIVE_ALL_BUSINESSES:
-            return merge({}, action.businesses);
+            return Object.assign({}, action.businesses);
         case RECEIVE_BUSINESS:
-            return merge({}, oldState, {[action.business.id]: action.business});
+            return Object.assign({}, oldState, {[action.business.id]: action.business});
         case RECEIVE_REVIEW:
             
             const {review} = action;
