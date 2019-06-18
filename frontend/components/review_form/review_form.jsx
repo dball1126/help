421e728b559a96538eb653e28cd224d28acb3e99
daniel <g.thirteen333@gmail.com>
2019-06-18 17:49:23 -0400

    review form star rating system complete

--- frontend/components/review_form/review_form.jsx
+++ frontend/components/review_form/review_form.jsx
@@ -100,21 +100,21 @@ class ReviewForm extends React.Component{
                                                     {/* className="fa fa-star" */}
                                                     <span className="rating select"> Select your rating</span>
                                                         
-                                                    <input type="radio" id="rating-input-1-5" className="rating-input" name="rating-input-1" value="1" onChange={this.update('rating')}/>
+                                                    <input type="radio" id="rating-input-1-5" className="rating-input" name="rating-input-1" value="5" onChange={this.update('rating')}/>
                                                     <label htmlFor="rating-input-1-5" className="star-box-rev" className="fa fa-star" ></label>
                                                     
                                                         
-                                                    <input type="radio" id="rating-input-1-4" className="rating-input" name="rating-input-1" value="2" onChange={this.update('rating')}/>
+                                                    <input type="radio" id="rating-input-1-4" className="rating-input" name="rating-input-1" value="4" onChange={this.update('rating')}/>
                                                     <label htmlFor="rating-input-1-4" className="star-box-rev" className="fa fa-star"></label>
                                                     
                                                     <input type="radio" id="rating-input-1-3" className="rating-input" name="rating-input-1" value="3" onChange={this.update('rating')}/>
                                                     <label htmlFor="rating-input-1-3" className="star-box-rev" className="fa fa-star"></label>
                                                     
                                                     
-                                                    <input type="radio" id="rating-input-1-2" className="rating-input" name="rating-input-1" value="4" onChange={this.update('rating')}/>
+                                                    <input type="radio" id="rating-input-1-2" className="rating-input" name="rating-input-1" value="1" onChange={this.update('rating')}/>
                                                     <label htmlFor="rating-input-1-2" className="star-box-rev" className="fa fa-star"></label>
                                                     
-                                                    <input type="radio" id="rating-input-1-1" className="rating-input" name="rating-input-1" value="5" onChange={this.update('rating')}/>
+                                                    <input type="radio" id="rating-input-1-1" className="rating-input" name="rating-input-1" value="1" onChange={this.update('rating')}/>
                                                     <label htmlFor="rating-input-1-1" className="star-box-rev" className="fa fa-star"></label>
                                                     
                                                     
