51c37d8a122130e5365356f49d0d776a0875b5b5
daniel <g.thirteen333@gmail.com>
2019-07-05 17:58:44 -0400

    updated search parameters

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,27 +1,51 @@
 class Api::SearchesController < ApplicationController
     def business_search
         query = params[:query][:query]
-        
-        if query == ""
-            @businesses = Business.all 
-            render '/api/businesses/index'
-        else
-        
-        queryString = query.split(' ').map do |string|
-            
-            string = "LOWER(name) LIKE '%#{string.downcase}%'"
-        end.join(" OR ");
-         
         location = params[:query][:location]
-        location = "new york" if location == "" 
-        location = location.split(',')[0]
-        locationStr = "LOWER(city) = '#{location.downcase}'" || "LOWER(state) = '#{location.downcase}'"
-
-        @businesses = Business.where('(' + queryString + ')')
+    #     if query == ""
+    #         @businesses = Business.all 
+    #         render '/api/businesses/index'
+    #     else
         
-        @businesses = Business.all if @businesses.empty?
-        render '/api/businesses/index'
-    end
+    #     queryString = query.split(' ').map do |string|
+            
+    #         string = "LOWER(name) LIKE '%#{string.downcase}%'"
+    #     end.join(" OR ");
+         
+    #     location = params[:query][:location]
+    #     location = "new york" if location == "" 
+    #     location = location.split(',')[0]
+    #     locationStr = "LOWER(city) = '#{location.downcase}'" || "LOWER(state) = '#{location.downcase}'"
+
+    #     @businesses = Business.where('(' + queryString + ')')
+        
+    #     @businesses = Business.all if @businesses.empty?
+    #     render '/api/businesses/index'
+    # end
+        if query == "" && location == ""
+            @businesses = Business.all
+            render '/api/businesses/index'
+        elsif query != "" && location == ""
+            debugger
+            queryString = query.split(' ').map do |string|
+                string = "LOWER(name) LIKE '%#{string.downcase}%'"
+            end.join(" OR ");
+            @businesses = Business.where('(' + queryString + ')')
+            render 'api/businesses/index'
+
+        elsif query == "" && location != ""
+            
+            queryString = location.split(' ').map do |string|
+                string = "LOWER(city) LIKE '%#{string.downcase}%'" + (" OR ") + ("LOWER(state) LIKE '%#{string.downcase}%'")
+            end.join(" OR ");
+            
+            @businesses = Business.where('(' + queryString + ')')            
+            render 'api/businesses/index'
+
+
+        end
+    
+    
     end
 
     def business_start
