4268c6e455ca7a4988642d3a6d3d6fded36f5998
daniel <g.thirteen333@gmail.com>
2019-07-25 15:19:52 -0400

    links on main page complete

--- frontend/components/greeting/homepage.jsx
+++ frontend/components/greeting/homepage.jsx
@@ -130,6 +130,17 @@ class Homepage extends React.Component {
 
             <div className="homepage-main-footer-container">
                 <div className="homepage-footer-inner-container">
+                    <div className="main-page-logo-box">
+                        <div className="main-page-img-box">
+                            <a href="https://www.linkedin.com/in/daniel-ball-1502b062/"><img src="https://yap-dev.s3.amazonaws.com/Linkedin-logo.png"/></a>
+                        </div>
+                        <div className="main-page-img-box">
+                            <a href="https://github.com/dball1126"><img src="https://yap-dev.s3.amazonaws.com/GitHub-logo-32px.png" /></a>
+                        </div>
+                        <div className="main-page-img-box">
+                            <a href="https://angel.co/daniel-ball-6"><img src="https://yap-dev.s3.amazonaws.com/AngelList_Black_Victory_Hand.png" /></a>
+                        </div>
+                    </div>
                     <div className="footer-img-wrapper">
                         <div className="footer-img"></div>
                     </div>
