d223f19b74473d79d5ed0ab0a8052e5d7a3ce433
daniel <g.thirteen333@gmail.com>
2019-07-25 16:07:44 -0400

    category migration complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_203812) do
+ActiveRecord::Schema.define(version: 2019_07_25_200240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 2019_06_05_203812) do
     t.datetime "updated_at", null: false
     t.index ["latitude", "longitude"], name: "index_businesses_on_latitude_and_longitude"
     t.index ["name"], name: "index_businesses_on_name", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "reviews", force: :cascade do |t|
