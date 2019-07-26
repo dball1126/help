550c1e47a3bfe83b05b9d1a77d126203d4048fba
daniel <g.thirteen333@gmail.com>
2019-07-26 15:35:12 -0400

    category api tested and approved and action created

---
+++ frontend/actions/category_actions.jsx
@@ -0,0 +1,34 @@
+export const RECEIVE_CATEGORY = 'RECEIVE_CATEGORY';
+export const RECEIVE_ALL_CATEGORIES = 'RECEIVE_ALL_CATEGORIES';
+import * as CategoryApIUtil from '../util/category_util';
+
+export const fetchCategory = (id) => {
+    return (dispatch) => {
+        return CategoryApIUtil.fetchCategory(id).then((payload) => {
+            return dispatch(receiveCategory(payload))
+        });
+    }
+}
+
+export const fetchCategories = () => {
+    return (dispatch) => {
+        return CategoryApIUtil.fetchCategories().then((payload) => {
+            return dispatch(receiveCategories(payload))
+        });
+    }
+};
+
+
+const receiveCategory = ({category}) => {
+    return {
+        type: RECEIVE_CATEGORY,
+        category: category
+    }
+}
+
+const receiveCategories = ({categories}) => {
+    return {
+        type: RECEIVE_ALL_CATEGORIES,
+        categories: categories
+    }
+}
