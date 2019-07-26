211d39e14adb19aa31b5471ead40ad7c0e67a3a7
daniel <g.thirteen333@gmail.com>
2019-07-26 15:08:26 -0400

    categories api created

---
+++ frontend/util/category_util.js
@@ -0,0 +1,12 @@
+export const fetchCategories = () => {
+    return $.ajax({
+        method: 'GET',
+        url: '/api/categories/'
+    })
+}
+export const fetchCategory = (id) => {
+    return $.ajax({
+        method: 'GET',
+        url: `/api/categories/${id}`
+    })
+}
