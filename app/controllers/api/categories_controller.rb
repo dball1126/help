eb9b26a61a4e41909119f5bcd57d52ed6896861c
daniel <g.thirteen333@gmail.com>
2019-07-26 14:22:57 -0400

    category model and jbuilder complete

---
+++ app/controllers/api/categories_controller.rb
@@ -0,0 +1,22 @@
+class Api::CategoriesController < ApplicationController
+
+    def new
+        @category = Category.new
+    end
+    
+    def create
+        @category = Category.create(category_params)
+        render :show
+    end
+    
+    def show
+        @category = Category.find(params[:id])
+        render :show
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+end
